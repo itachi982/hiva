9735a72398eb3c2c561171f35d0426aa8045de4f
vishal patsariya <vishalpatsariya99@gmail.com>
2024-03-20 15:29:04 +0530

    minor update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,12 +4,12 @@ import './App.css'
 import { UserSignin } from './pages/UserSignin'
 import { AdminSignup } from './pages/AdminSignup'
 import { BrowserRouter, Route,Routes } from 'react-router-dom'
-import { Footer } from './components/Footer'
 import { UserDashboard } from './pages/Userdashboard'
 import { AdminDashboard } from './pages/AdminDashboard'
-import { Home } from './pages/Home'
+import { UserHome } from './pages/UserHome'
 import { AdminSignin } from './pages/AdminSignin'
 import { Navigate } from 'react-router-dom'
+import { AdminHome } from './pages/AdminHome'
 
 
 function App() {
@@ -27,15 +27,17 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/home" element={<Home/>} />
+          <Route path="/" element={<UserHome/>} />
+          <Route path="/home" element={<UserHome/>} />
+
+          <Route path="/admin" element={<AdminHome/>} />
           <Route path="/admin/signin" element={<AdminSignin setIsAdmin={setIsAdmin}/>} />
           <Route path="/admin/signup" element={<AdminSignup/>} />            
+          <Route path="/admin/dashboard" element={<AdminDashboard/>}/>
           {/* <Route path="/admin/dashboard" element={isAdmin ? <AdminDashboard /> : <Navigate to="/admin/signin" />} /> */}
+          
           <Route path="/employee/signin" element={<UserSignin setIsUser={setIsUser}/>} />
           <Route path="/employee/dashboard" element={isUser ? <AdminDashboard /> : <Navigate to="/employee/signin" />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard/>}/>
-          <Route path="/footer" element={<Footer/>}/>
           
         </Routes>
       </BrowserRouter>
--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -1,36 +1,46 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import bannerimg from "../assets/banner.png"
+import { Footer } from "./Footer";
 
 
 export const Banner = () => {
     return (
-        <div className="pt-14 parent min-h-[100vh] flex flex-col-reverse lg:flex-row items-center justify-between bg-slate-300">
-            <motion.div
-                initial={{ x: "-100vw" }}
-                animate={{ x: 0 }}
-                transition={{ duration: 1 }}
-            >
-                <h1 className="text-2xl font-semibold ">Salary and Attendance Management Tool</h1>
-                <br/>
-                <p className="font-light ">A business-focused platform designed to streamline and 
-                    integrate the employee payroll management process efficiently using digital technology.</p>
-    
-            </motion.div>
-            <motion.div
-                className="w-full md:w-1/3"
-                initial={{ x: "100vw" }}
-                animate={{ x: 0 }}
-                transition={{ duration: 1 }}
-            >
-                <div className="order-1  lg:order-3 lg:pt-0 md:pt-0 sm:pt-0">
-                    <img
-                        src={bannerimg}
-                        title="Hiva Banner"
-                        alt="Hiva Banner"
-                    />
-                </div>
-            </motion.div>
+        <div>
+            <div className="parent h-auto pt-40 pb-40 flex flex-col-reverse lg:flex-row items-center justify-between bg-slate-300">
+                <motion.div
+                    initial={{ x: "-100vw" }}
+                    animate={{ x: 0 }}
+                    transition={{ duration: 1 }}
+                >
+                    <h1 className="text-2xl font-semibold ">Salary and Attendance Management Tool</h1>
+                    <br/>
+                    <p className="font-light ">A business-focused platform designed to streamline and 
+                        integrate the employee payroll management process efficiently using digital technology.</p>
+        
+                </motion.div>
+                <motion.div
+                    className="w-full md:w-1/3"
+                    initial={{ x: "100vw" }}
+                    animate={{ x: 0 }}
+                    transition={{ duration: 1 }}
+                >
+                    <div className="order-1  lg:order-3 lg:pt-0 md:pt-0 sm:pt-0">
+                        <img
+                            src={bannerimg}
+                            title="Hiva Banner"
+                            alt="Hiva Banner"
+                        />
+                    </div>
+                </motion.div>
+            </div>
+            <div className="bg-slate-300">
+                <motion.div className="bg-slate-300" initial={{ x: "100vw" }} animate={{ x: 0 }} transition={{ duration: 1 }}>
+                    <div className=" bg-slate-300">
+                        <Footer/>
+                    </div>
+                </motion.div>
+            </div>
         </div>
     );
 }
