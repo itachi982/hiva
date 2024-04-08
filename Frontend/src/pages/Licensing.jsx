6d531c84a9029c721f2426747041d5466c83a458
vishal patsariya <vishalpatsariya99@gmail.com>
2024-04-08 13:51:46 +0530

    Update Licensing.jsx

--- Frontend/src/pages/Licensing.jsx
+++ Frontend/src/pages/Licensing.jsx
@@ -16,85 +16,30 @@ export const LicensingContent = () => {
     return (    
     <div>
             <div className=" bg-slate-300">
-                <UserNavbar />
-                    <div className="text-gray-500 flex justify-center mt-10 text-center font-medium text-5xl">
-                         Support
-                    </div>
-            
-                    <div className="border border-gray-300 rounded-lg p-4 mt-8">
-    <div className="mb-4">
-        <p className="font-bold">Community support</p>
-        <p>
-            As an open source platform, Budibase offers community support to everyone. This includes responding to GitHub Issues, answering GitHub Discussions, and chatting across a number of public channels on our Discord Server.
-        </p>
-        <p>
-            When interacting with community support you may receive help from either our Customer Success team, or another supportive community member. While this support is not obligated, the Budibase team endeavors to answer what we can, and all community contributions are appreciated!
-        </p>
-        <p>
-            The GitHub repository is a good place to look for FAQs.
-        </p>
-    </div>
+                <UserNavbar />        
+            </div>
+            <div className="bg-slate-300 h-200 p-40 text-xl font-semibold text-gray-600 text-center">
+                <p className="text-center">MIT License</p><br/>
 
-    <div className="mb-4">
-        <p className="font-bold">Premium support</p>
-        <p>
-            In addition to having all of the community resources available, users on the Premium plan can contact support directly via email. This provides a private communication channel for support.
-        </p>
-        <p className="font-bold">What is supported?</p>
-        <ul className="list-disc ml-8">
-            <li>✓ Answering questions about building Budibase apps</li>
-            <li>✓ Investigating issues relating to the Budibase platform</li>
-            <li>✓ Guidance on building custom plugins</li>
-            <li>✓ Premium-tier licensed instances</li>
-            <li>✓ The most recent version of Budibase</li>
-        </ul>
-        <p className="font-bold">Not supported</p>
-        <ul className="list-disc ml-8">
-            <li>- Infrastructure support for self-hosted users</li>
-            <li>- Maintaining custom plugins</li>
-            <li>- Debugging code, such as JavaScript and SQL, etc</li>
-            <li>- Community / open-source licensed instances</li>
-        </ul>
-    </div>
+                <p className="text-center">Copyright (c) 2022 Vishal Patsariya and Chitresh Manjhi</p><br/>
 
-    <div className="mb-4">
-        <p className="font-bold">Enterprise support</p>
-        <p>
-            The Enterprise tier provides your business with prompt priority support. Furthermore, an Account Manager will be assigned to assist with onboarding.
-        </p>
-        <p>
-            You can view the additional features included in Enterprise on the pricing page.
-        </p>
-        <p className="font-bold">What is supported?</p>
-        <ul className="list-disc ml-8">
-            <li>✓ Answering questions about building Budibase apps</li>
-            <li>✓ Investigating issues relating to the Budibase platform</li>
-            <li>✓ Guidance on building custom plugins</li>
-            <li>✓ Onboarding</li>
-            <li>✓ Infrastructure support for self-hosted users</li>
-            <li>✓ Enterprise licensed instances</li>
-            <li>✓ The last 5 minor versions of Budibase</li>
-        </ul>
-        <p className="font-bold">Not supported</p>
-        <ul className="list-disc ml-8">
-            <li>- Maintaining custom plugins</li>
-            <li>- Debugging code, such as JavaScript and SQL, etc</li>
-            <li>- Community/open-source licensed instances</li>
-        </ul>
-    </div>
+                <p className="text-center">Permission is hereby granted, free of charge, to any person obtaining a copy
+                of this software and associated documentation files (the "Software"), to deal
+                in the Software without restriction, including without limitation the rights
+                to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                copies of the Software, and to permit persons to whom the Software is
+                furnished to do so, subject to the following conditions:</p><br/>
 
-    <div className="mb-4">
-        <p className="font-bold">SLA</p>
-        <ul className="list-disc ml-8">
-            <li>✓ Response within two business days</li>
-        </ul>
-    </div>
-</div>
+                <p>The above copyright notice and this permission notice shall be included in all
+                copies or substantial portions of the Software.</p><br/>
 
-
-                    
-                   
-                
+                <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                SOFTWARE.</p><br/>
             </div>
             <div>
                <Footer/>
